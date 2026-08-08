0f3e33c79c0bb265b34aec52854bf71378d457be
thuanvo2405 <vothuan2405003@gmail.com>
2026-08-08 20:55:08 +0700

    TextNode Tests

--- src/textnode.py
+++ src/textnode.py
@@ -11,7 +11,7 @@ class TextType(Enum):
 
 
 class TextNode:
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -22,7 +22,7 @@ class TextNode:
         return (
             self.text == other.text
             and self.text_type == other.text_type
-            and self.url == other.text_type
+            and self.url == other.url
         )
 
     def __repr__(self) -> str:
