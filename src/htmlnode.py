4929f8ab124b13cd161fa59e7336831ccce474fb
thuanvo2405 <vothuan2405003@gmail.com>
2026-08-08 21:10:47 +0700

    HTMLNode

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None) -> None:
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError()
+
+    def props_to_html(self):
+        if not self.props:
+            return ""
+        props_html = ""
+        for key, value in self.props.items():
+            props_html += f' {key}="{value}"'
+        return props_html
+
+    def __repr__(self) -> str:
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
