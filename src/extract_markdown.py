a20bbded94159a289703cebb6198137bdf26f512
thuanvo2405 <vothuan2405003@gmail.com>
2026-08-10 15:32:16 +0700

    Split Blocks

--- src/extract_markdown.py
+++ src/extract_markdown.py
@@ -9,3 +9,13 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
+
+
+def markdown_to_blocks(markdown):
+    new_list = []
+    blocks = markdown.split("\n\n")
+    for block in blocks:
+        block = block.strip()
+        if block != "":
+            new_list.append(block)
+    return new_list
--- src/test_extract_markdown.py
+++ src/test_extract_markdown.py
@@ -1,6 +1,10 @@
 import unittest
 
-from extract_markdown import extract_markdown_images, extract_markdown_links
+from extract_markdown import (
+    extract_markdown_images,
+    extract_markdown_links,
+    markdown_to_blocks,
+)
 
 
 class TestExtractMarkdown(unittest.TestCase):
@@ -39,6 +43,26 @@ class TestExtractMarkdown(unittest.TestCase):
             "This is text with an ![image](https://i.imgur.com/zkkcJKZ.png)"
         )
         self.assertNotEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+    """
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
 
 
 if __name__ == "__main__":
