f09119b251896a243008b4951069c9a0828bc762
thuanvo2405 <vothuan2405003@gmail.com>
2026-08-13 11:37:58 +0700

    Copy Static

--- src/main.py
+++ src/main.py
@@ -1,9 +1,33 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
 
 def main() -> None:
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    copy_static_to_public()
+
+
+def copy_static_to_public():
+    public_path = "./public"
+    if os.path.exists(os.path.join("./", "public")):
+        shutil.rmtree(public_path)
+
+    os.mkdir(public_path)
+    copy_path_and_file("./static", "./public")
+
+
+def copy_path_and_file(soucre_path, receive_path):
+    list = os.listdir(soucre_path)
+    for item in list:
+        if os.path.isfile(os.path.join(soucre_path, item)):
+            print(os.path.join(soucre_path, item))
+            shutil.copy(
+                os.path.join(soucre_path, item), os.path.join(receive_path, item)
+            )
+        else:
+            os.mkdir(f"{receive_path}/{item}")
+            copy_path_and_file(
+                os.path.join(soucre_path, item), os.path.join(receive_path, item)
+            )
 
 
 main()
